4c7bf19455f1287778bdab441b0455df3d69bde8
swol1 <den.gavrilin1@gmail.com>
2019-11-10 17:59:37 +0300

    Updates

---
+++ precache-manifest.883460359e1b0d7ee520b4eb605d29f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d0a73de36a3556cadcc8890fc617968",
+    "url": "/react-paint/index.html"
+  },
+  {
+    "revision": "cc4b2465210f7d4a1488",
+    "url": "/react-paint/static/css/main.b74591dd.chunk.css"
+  },
+  {
+    "revision": "23e0298c1c191c58981e",
+    "url": "/react-paint/static/js/2.4ec8b5d0.chunk.js"
+  },
+  {
+    "revision": "cc4b2465210f7d4a1488",
+    "url": "/react-paint/static/js/main.bd5f22ec.chunk.js"
+  },
+  {
+    "revision": "09be4ff5886d042cf01c",
+    "url": "/react-paint/static/js/runtime-main.4cf3ce49.js"
+  }
+]);
